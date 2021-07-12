24ca627c9da022d6beba57fa6d9130662ee6e179
Michael0714 <2488976815@qq.com>
2021-07-12 14:45:59 +0800

    Day 1
    Hello World~

---
+++ src/fundamental_knowledge/demo01.java
@@ -0,0 +1,8 @@
+package fundamental_knowledge;
+
+public class demo01 {
+    // 1. Hello World  我们需要在main函数里面实现想要的功能, 例如 打印Hello World~
+    public static void main(String[] args){
+        System.out.print("Hello World~");
+    }
+}
