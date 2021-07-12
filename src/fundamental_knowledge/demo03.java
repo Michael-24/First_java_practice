30cf74acf57a25c8e1d35e7bf96bca3b56afd365
Michael-24 <62531813+Michael-24@users.noreply.github.com>
2021-07-12 18:25:27 +0800

    Update demo03.java

--- src/fundamental_knowledge/demo03.java
+++ src/fundamental_knowledge/demo03.java
@@ -9,7 +9,7 @@ public class demo03 {
             for (int j = 1; j <= i; j++) { // 控制列
                 System.out.print(j + "*" + i + "=" + j * i + "\t");
             }
+            System.out.println(); // 输入一行后就换行
         }
-        System.out.println(); // 输入一行后就换行
     }
 }
