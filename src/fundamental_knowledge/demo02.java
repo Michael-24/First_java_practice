c9e12ac3714c78576831df1c6e7012ea9cb2274b
Michael0714 <2488976815@qq.com>
2021-07-12 16:32:54 +0800

    Day 02
    在这里介绍了些java的基本知识，通过简单的知识清单以及代码的学习，快速梳理一遍吧~

---
+++ src/fundamental_knowledge/demo02.java
@@ -0,0 +1,22 @@
+package fundamental_knowledge;
+
+public class demo02 {
+    // 数组及其使用方法
+    public static void main(String[] args){
+        // 八大数据类型
+        int num = 10;
+        byte num2 = 20;
+        short num3 = 30;
+        long num4 = 30L;
+
+        float num5 = 30.2F;// 定义float类型时要在数字后加 F/f
+        double num6 = 23213.123323;
+        char A = 'q'; // 字符
+
+        String name = "Michael";
+
+        final double num00 = 123412; // final修饰的常量不可修改
+
+        System.out.print(num6);
+    }
+}
